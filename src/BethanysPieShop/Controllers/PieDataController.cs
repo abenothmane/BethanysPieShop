de3fd4e7d53203638647d0058fd1ac129ac1c6a0
Anas <num_ein2@hotmail.com>
2017-04-09 17:15:09 -0700

    Adding Client side functionalities: - added Jquery dependencies - dynamic listing of the objects

---
+++ src/BethanysPieShop/Controllers/PieDataController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using BethanysPieShop.Models;
+using BethanysPieShop.ViewModels;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BethanysPieShop.Controllers
+{
+    [Route("api/[controller]")]
+    public class PieDataController : Controller
+    {
+        private readonly IPieRepository _pieRepository;
+
+        public PieDataController(IPieRepository pieRepository )
+        {
+            _pieRepository = pieRepository;
+        }
+
+
+        [HttpGet]
+        public IEnumerable<PieViewModel> LoadMorePies()
+        {
+            IEnumerable<Pie> dbPies = dbPies = _pieRepository.Pies.OrderBy(p => p.PieId).Take(10);
+
+            List<PieViewModel> pies = new List<PieViewModel>();
+
+            foreach (var dbpie in dbPies)
+            {
+                pies.Add(MapDbPieToPieViewModel(dbpie));
+            }
+
+            return pies;
+
+        }
+
+        public PieViewModel MapDbPieToPieViewModel(Pie pie)
+        {
+            return new PieViewModel() {
+
+                PieId = pie.PieId,
+                Name = pie.Name,
+                Price = pie.Price,
+                ShortDescription = pie.ShortDescription,
+                ImageThumbnailUrl = pie.ImageThumbnailUrl
+            };
+        }
+
+
+    }
+}
